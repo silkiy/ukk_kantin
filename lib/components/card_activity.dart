5cc66c437aabe285dd12016149ba3bba7b1b24fd
silkiy <wildanmilk@gmail.com>
2025-03-02 00:05:17 +0700

    fix : CardActivity

--- lib/components/card_activity.dart
+++ lib/components/card_activity.dart
@@ -1,229 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:ukk_kantin_epan/pages/student/detail_order_page.dart';
 
-class CardActivity extends StatelessWidget {
-  const CardActivity({
-    super.key,
-  });
+class CardActivity extends StatefulWidget {
+  const CardActivity({super.key});
 
   @override
+  State<CardActivity> createState() => _CardActivityState();
+}
+
+class _CardActivityState extends State<CardActivity> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.5,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color:
-                const Color.fromRGBO(158, 158, 158, 1).withValues(alpha: 0.5),
-            spreadRadius: 3,
-            blurRadius: 5,
-            offset: Offset(0, 2),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailOrderPage(),
           ),
-        ],
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 20,
+        );
+      },
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.13,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(112, 92, 233, 0.2),
+          borderRadius: BorderRadius.circular(16),
         ),
-        child: Column(
+        padding: EdgeInsets.all(12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.width * 0.1,
-                      width: MediaQuery.of(context).size.width * 0.1,
-                      margin: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Color.fromRGBO(112, 92, 233, 1),
-                          width: 2.5,
-                        ),
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "1x",
-                          style: GoogleFonts.nunitoSans(
-                            fontSize: MediaQuery.of(context).size.width * 0.05,
-                            fontWeight: FontWeight.w600,
-                            color: Color.fromRGBO(112, 92, 233, 1),
-                          ),
-                        ),
-                      ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Bu Darti",
+                    style: TextStyle(
+                      color: Color(0xff705CE9),
+                      fontSize: MediaQuery.of(context).size.width * 0.045,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                      "Nasi Telor",
-                      style: GoogleFonts.nunitoSans(
-                        fontSize: MediaQuery.of(context).size.width * 0.035,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.black,
-                      ),
-                    )
-                  ],
-                ),
-                Text(
-                  "10.000",
-                  style: GoogleFonts.nunitoSans(
-                    fontSize: MediaQuery.of(context).size.width * 0.035,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.width * 0.1,
-                      width: MediaQuery.of(context).size.width * 0.1,
-                      margin: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Color.fromRGBO(112, 92, 233, 1),
-                          width: 2.5,
-                        ),
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "1x",
-                          style: GoogleFonts.nunitoSans(
-                            fontSize: MediaQuery.of(context).size.width * 0.05,
-                            fontWeight: FontWeight.w600,
-                            color: Color.fromRGBO(112, 92, 233, 1),
-                          ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.005),
+                  Text(
+                    "Total : Rp10.000",
+                    style: TextStyle(
+                      color: Color(0xff705CE9),
+                      fontSize: MediaQuery.of(context).size.width * 0.035,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.005),
+                  Row(
+                    children: [
+                      Text(
+                        "26 Jan 2025 ",
+                        style: TextStyle(
+                          color: Color(0xff705CE9),
+                          fontSize: MediaQuery.of(context).size.width * 0.03,
                         ),
                       ),
-                    ),
-                    Text(
-                      "Es Teh",
-                      style: GoogleFonts.nunitoSans(
-                        fontSize: MediaQuery.of(context).size.width * 0.035,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.black,
+                      Text(
+                        "10:12 AM",
+                        style: TextStyle(
+                          color: Color(0xff705CE9),
+                          fontSize: MediaQuery.of(context).size.width * 0.03,
+                        ),
                       ),
-                    )
-                  ],
-                ),
-                Text(
-                  "3.000",
-                  style: GoogleFonts.nunitoSans(
-                    fontSize: MediaQuery.of(context).size.width * 0.035,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
+                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-            SizedBox(height: 30),
-            Divider(
-              thickness: 0.5,
-              color: Colors.grey[400],
-            ),
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text(
-                  "Subtotal(Incl. tax)",
-                  style: GoogleFonts.nunitoSans(
-                    fontSize: MediaQuery.of(context).size.width * 0.03,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                  ),
-                ),
-                Text(
-                  "13.000",
-                  style: GoogleFonts.nunitoSans(
-                    fontSize: MediaQuery.of(context).size.width * 0.03,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 5),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Order fee",
-                  style: GoogleFonts.nunitoSans(
-                    fontSize: MediaQuery.of(context).size.width * 0.03,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                  ),
-                ),
-                Text(
-                  "2.000",
-                  style: GoogleFonts.nunitoSans(
-                    fontSize: MediaQuery.of(context).size.width * 0.03,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.local_offer_outlined,
-                      color: Color.fromRGBO(112, 92, 233, 1),
-                      size: 30,
-                    ),
-                    Text(
-                      "Use Discount",
-                      style: GoogleFonts.nunitoSans(
-                        fontSize: MediaQuery.of(context).size.width * 0.035,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.black,
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailOrderPage(),
                       ),
-                    )
-                  ],
-                ),
-                Icon(
-                  Icons.arrow_forward_ios,
-                  size: 20,
-                ),
-              ],
-            ),
-            SizedBox(height: 50),
-            Divider(
-              thickness: 0.5,
-              color: Colors.grey[400],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Total",
-                  style: GoogleFonts.nunitoSans(
-                    fontSize: MediaQuery.of(context).size.width * 0.04,
-                    fontWeight: FontWeight.w800,
-                    color: Colors.black,
+                    );
+                  },
+                  icon: Icon(
+                    Icons.chevron_right,
+                    color: Color(0xff705CE9),
+                    size: MediaQuery.of(context).size.width * 0.06,
                   ),
                 ),
-                Text(
-                  "15.000",
-                  style: GoogleFonts.nunitoSans(
-                    fontSize: MediaQuery.of(context).size.width * 0.04,
-                    fontWeight: FontWeight.w800,
-                    color: Colors.black,
+                SizedBox(height: MediaQuery.of(context).size.height * 0.005),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.04,
+                    vertical: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                  child: Text(
+                    "Dimasak",
+                    style: TextStyle(
+                      color: Color(0xff705CE9),
+                      fontWeight: FontWeight.bold,
+                      fontSize: MediaQuery.of(context).size.width * 0.035,
+                    ),
                   ),
                 ),
               ],
