c5042049a95d782d5b45c58bab2ad74d13053fca
silkiy <wildanmilk@gmail.com>
2025-03-02 00:06:57 +0700

    fix : add HomePageVendor

---
+++ lib/pages/vendor/home_page_vendor.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePageVendor extends StatefulWidget {
+  const HomePageVendor({super.key});
+
+  @override
+  State<HomePageVendor> createState() => _HomePageVendorState();
+}
+
+class _HomePageVendorState extends State<HomePageVendor> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+      
+    );
+  }
+}
